2b032307cce566cd357b7857d410b3fb1c9bb56e
aseel <aseel@aseel.localdomain>
2021-04-09 16:57:28 +0200

    added test

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 
 class LoginController extends Controller
@@ -18,26 +19,20 @@ class LoginController extends Controller
      */
     public function __invoke(Request $request)
     {
+        // $credentials = [
+        //     'email' => $request['email'],
+        //     'password' => $request['password'],
 
-        // $credentials = $request->only(['email', 'password']);
+        // ];
+        $credentials = $request->only(['email', 'password']);
 
-        $credentials =  $this->validate($request, [
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
-
+        dd($credentials);
         if (Auth::attempt($credentials)) {
 
-            // Auth::login();
 
-            return redirect('/dashboard');
+            return redirect('dashboard');
         }
+
         return back()->withErrors('Whoops! Please try to login again.');
     }
-
-
-    //
-
-    // // Login and "remember" the given user...
-    // Auth::login($user, true);
 }
